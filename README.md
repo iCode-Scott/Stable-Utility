45d47e0f1ea38636193423ea404e1a33f06c58c0
iCode ของ สก็อต <127704775+iCode-Scott@users.noreply.github.com>
2023-03-13 13:15:40 +0700

    Add feature

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Stable-utility
+# Stable Utility
 A Windows batch script to perform various system tasks for Windows 10 Stable version. This script requires administrative privileges to execute.
+## Features
+The Stable Utility script provides the following features:
+- Debloat services
+- Enable low latency mode
+- Advanced Clean-up Temporary files
+- Run Advanced temporary cleanup
+- Delete Empty folder
+- Do all task
+## Task Options
+### Debloat services
+This task allows you to disable certain services to make Windows 10 more stable.
+### Enable low latency mode
+This task allows you to configure the High Precision Event Timer. You can choose to disable it. To get Low latency on Windows 10.
+### Advanced Clean-up Temporary files
+This task allows you to clean up temporary files from various locations on your system.
+### Delete Empty folder
+This task will delte all empty folder to Fixed 100% HDD/SSD
+## Disclaimer
+This script is provided as-is and without warranty. Use at your own risk.
---
+++ uac_disable.cmd
@@ -0,0 +1,262 @@
+@echo off
+setlocal
+color 0f
+title Stable Utility
+:ask_for_create_system_restore_point
+cls
+echo Let create system restore point(Used to undo all changes and restore if device brick)
+echo.
+echo.
+echo.
+set /p choice=Type "Y" to continue or "N" to cancel and press Enter: 
+
+if /i "%choice%" == "Y" (
+    cls
+    echo Create system restore point, please wait...
+    wmic.exe /Namespace:\\root\default Path SystemRestore Call CreateRestorePoint "Stable Utility", 100, 7 > nul 2>&1
+    cls
+    echo System restore point created successfully.
+    timeout /t 5 /nobreak > nul
+) else (
+    cls
+    echo System restore point creation cancelled.
+    timeout /t 5 /nobreak > nul
+)
+goto menu
+
+:menu
+cls
+echo Script to perform various system tasks for Windows 10 Stable version.
+echo Here is Task list:
+echo.
+echo.
+echo 1 - Debloat services
+echo 2 - Enable low latency mode
+echo 3 - Run advanced temporary cleanup
+echo 4 - Delete empty folder
+echo 5 - Disabled memory compression
+echo 6 - Do all task
+echo.
+echo.
+echo.
+
+set /p choice=Type the number of the task you want to perform and press Enter: 
+
+if "%choice%" == "1" (
+    call :debloat_services
+    goto :menu
+)
+
+if "%choice%" == "2" (
+    call :enable_low_latency_mode
+    goto :menu
+)
+
+if "%choice%" == "3" (
+    call :run_advanced_temporary_cleanup
+    goto :menu
+)
+
+if "%choice%" == "4" (
+    call :delete_empty_folder
+    goto :menu
+)
+
+if "%choice%" == "5" (
+    call :disabled_memory_compression
+    goto :menu
+)
+if "%choice%" == "6" (
+    call :do_all_task
+    goto :menu
+)
+
+:debloat_services
+cls
+echo Debloat services
+echo.
+echo.
+echo.
+
+set /p choice=Type "Y" to continue or "N" to cancel and press Enter: 
+if /i "%choice%" == "Y" (
+    cls
+    echo Debloat services, please wait...
+    net stop "BITS" > nul 2>&1
+    sc config "BITS" start=disabled > nul 2>&1
+    net stop "SysMain" > nul 2>&1
+    sc config "SysMain" start=disabled > nul 2>&1
+    net stop "Schedule" > nul 2>&1
+    sc config "Schedule" start=disabled > nul 2>&1
+    net stop "TimeBrokerSvc" > nul 2>&1
+    sc config "TimeBrokerSvc" start=disabled > nul 2>&1 
+    reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v "Ctfmon" /t REG_SZ /d "C:\Windows\System32\ctfmon.exe"
+    net stop "Themes" > nul 2>&1
+    sc config "Themes" start=disabled > nul 2>&1
+    net stop "WSearch" > nul 2>&1
+    sc config "WSearch" start=disabled > nul 2>&1
+    cls
+    echo Debloat services successfully.
+    timeout /t 5 /nobreak > nul
+) else (
+    cls
+    echo Debloat services cancelled.
+    timeout /t 5 /nobreak > nul
+)
+goto menu
+
+:enable_low_latency_mode
+cls
+echo Enable low latency mode
+echo.
+echo.
+echo.
+
+set /p choice=Type "Y" to continue or "N" to cancel and press Enter: 
+if /i "%choice%" == "Y" (
+    cls
+    echo Enable low latency mode, please wait...
+    bcdedit /deletevalue useplatformclock > nul 2>&1
+    bcdedit /set disabledynamictick yes > nul 2>&1
+    cls
+    echo Enable low latency mode successfully.
+    timeout /t 5 /nobreak > nul
+) else (
+    cls
+    echo Enable low latency mode cancelled.
+    timeout /t 5 /nobreak > nul
+)
+goto menu
+
+:run_advanced_temporary_cleanup
+cls
+echo Run advanced temporary cleanup
+echo.
+echo.
+echo.
+
+set /p choice=Type "Y" to continue or "N" to cancel and press Enter: 
+if /i "%choice%" == "Y" (
+    cls
+    echo Run advanced temporary cleanup, please wait...
+    del /F /S /Q %temp% > nul 2>&1
+    del /F /S /Q "C:\Windows\Temp" > nul 2>&1
+    del /F /S /Q "C:\Windows\Prefetch" > nul 2>&1
+    del /F /S /Q "C:\Windows\SoftwareDistribution\Download" > nul 2>&1
+    del /F /S /Q "C:\Windows\SoftwareDistribution\SLS" > nul 2>&1
+    del /F /S /Q "" > nul 2>&1
+    start %windir%cleanmgr.exe /AUTOCLEAN > nul 2>&1
+    cls
+    echo Run advanced temporary cleanup successfully.
+    timeout /t 5 /nobreak > nul
+) else (
+    cls
+    echo Run advanced temporary cleanup cancelled.
+    timeout /t 5 /nobreak > nul
+)
+goto menu
+
+:delete_empty_folder
+cls
+echo Delete empty folder
+echo.
+echo.
+echo.
+
+set /p choice=Type "Y" to continue or "N" to cancel and press Enter: 
+if /i "%choice%" == "Y" (
+    cls
+    echo Delete empty folder, pleas wait...
+    set "rootFolder=C:"
+    for /f "delims=" %%d in ('dir /ad/b/s "%rootFolder%" ^| sort /r') do (
+        rd "%%d" 2>nul "%%d"
+    )
+    cls
+    echo Delete empty folder successfully.
+    timeout /t 5 /nobreak > nul
+) else (
+    cls
+    echo Delete empty folder cancelled.
+    timeout /t 5 /nobreak > nul
+)
+goto menu
+
+:disabled_memory_compression
+cls
+echo Disabled memory compression
+echo.
+echo.
+echo.
+
+set /p choice=Type "Y" to continue or "N" to cancel and press Enter:
+if /i "%choice%" == "Y" (
+    cls
+    echo
+    Disabled memory compression, please wait...
+    powershell Disable-MMAgent -mc > nul 2>&1
+    cls
+    echo Disabled memory compression successfully.
+    timeout /t 5 /nobreak > nul
+) else (
+    cls
+    echo Disabled memory compression cancelled.
+    timeout /t 5 /nobreak > nul
+)
+goto menu
+
+:do_all_task
+cls
+echo Do all task
+echo.
+echo.
+echo.
+
+set /p choice=Type "Y" to continue or "N" to cancel and press Enter: 
+if /i "%choice%" == "Y" (
+    cls
+    echo Do all task, please wait...
+    net stop "BITS" > nul 2>&1
+    sc config "BITS" start=disabled > nul 2>&1
+    net stop "SysMain" > nul 2>&1
+    sc config "SysMain" start=disabled > nul 2>&1
+    net stop "Schedule" > nul 2>&1
+    sc config "Schedule" start=disabled > nul 2>&1
+    net stop "TimeBrokerSvc" > nul 2>&1
+    sc config "TimeBrokerSvc" start=disabled > nul 2>&1
+    reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v "Ctfmon" /t REG_SZ /d "C:\Windows\System32\ctfmon.exe" 
+    net stop "Themes" > nul 2>&1
+    sc config "Themes" start=disabled > nul 2>&1
+    net stop "WSearch" > nul 2>&1
+    sc config "WSearch" start=disabled > nul 2>&1
+    echo Debloat services successfully.
+    bcdedit /deletevalue useplatformclock > nul 2>&1
+    bcdedit /set disabledynamictick yes > nul 2>&1
+    echo Enable low latency mode successfully.
+    del /F /S /Q %temp% > nul 2>&1
+    del /F /S /Q "C:\Windows\Temp" > nul 2>&1
+    del /F /S /Q "C:\Windows\Prefetch" > nul 2>&1
+    del /F /S /Q "C:\Windows\SoftwareDistribution\Download" > nul 2>&1
+    del /F /S /Q "C:\Windows\SoftwareDistribution\SLS" > nul 2>&1
+    del /F /S /Q "" > nul 2>&1
+    start %windir%cleanmgr.exe /AUTOCLEAN > nul 2>&1
+    echo Run advanced temporary cleanup successfully.
+    set "rootFolder=C:"
+    for /f "delims=" %%d in ('dir /ad/b/s "%rootFolder%" ^| sort /r') do (
+        rd "%%d" 2>nul "%%d"
+    )
+    echo Delete empty folder successfully.
+    powershell Disable-MMAgent -mc > nul 2>&1
+    echo Disabled memory compression successfully.
+    cls
+    echo Debloat services successfully.
+    echo Enable low latency mode successfully.
+    echo Run advanced temporary cleanup successfully.
+    echo Delete empty folder successfully.
+    echo disabled memory compression successfully.
+    timeout /t 5 /nobreak > nul
+) else (
+    cls
+    echo Do all task cancelled.
+    timeout /t 5 /nobreak > nul
+)
+goto menu
